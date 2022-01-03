4b677cff45e413be40aaf8e5a2b58c335978c4fa
Vincent Horecký <vincent.horecky@seznam.cz>
2022-01-03 17:28:31 +0100

    Zprovoznění btn Objem

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,21 @@
 window.onload = function () {
   let slctDelka = document.getElementById("slctDelka");
   let slctHmotnost = document.getElementById("slctHmotnost");
+  let slctObjem = document.getElementById("slctObjem");
   let btnDelka = document.getElementById("btnDelka");
   let btnHmotnost = document.getElementById("btnHmotnost");
+  let btnObjem = document.getElementById("btnObjem");
   slctDelka.style.display = "none";
   slctHmotnost.style.display = "none";
+  slctObjem.style.display = "none";
 
   btnDelka.onclick = function () {
     slctDelka.style.display = "inline";
     btnDelka.className = "one aktivni";
     btnHmotnost.className = "two neaktivni";
     slctHmotnost.style.display = "none";
+    slctObjem.style.display = "none";
+    btnObjem.className = "three neaktivni";
   };
 
   btnHmotnost.onclick = function () {
@@ -18,5 +23,16 @@ window.onload = function () {
     btnDelka.className = "one neaktivni";
     btnHmotnost.className = "two aktivni";
     slctHmotnost.style.display = "inline";
+    slctObjem.style.display = "none";
+    btnObjem.className = "three neaktivni";
+  };
+
+  btnObjem.onclick = function () {
+    slctObjem.style.display = " inline";
+    slctDelka.style.display = "none";
+    slctHmotnost.style.display = "none";
+    btnObjem.className = "three aktivni";
+    btnDelka.className = "one neaktivni";
+    btnHmotnost.className = "two neaktivni";
   };
 };
