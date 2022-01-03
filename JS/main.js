3434a34414cbafd0bacf0073e169890333cb26ef
Vincent Horecký <vincent.horecky@seznam.cz>
2022-01-03 18:42:56 +0100

    Zprovoznění obsahu

--- JS/main.js
+++ JS/main.js
@@ -2,34 +2,53 @@ window.onload = function () {
   let slctDelka = document.getElementById("slctDelka");
   let slctHmotnost = document.getElementById("slctHmotnost");
   let slctObjem = document.getElementById("slctObjem");
+  let slctObsah = document.getElementById("slctObsah");
   let btnDelka = document.getElementById("btnDelka");
   let btnHmotnost = document.getElementById("btnHmotnost");
   let btnObjem = document.getElementById("btnObjem");
+  let btnObsah = document.getElementById("btnObsah");
 
   btnDelka.onclick = function () {
     slctDelka.style.display = "inline";
-    btnDelka.className = "one aktivni";
-    btnHmotnost.className = "two neaktivni";
     slctHmotnost.style.display = "none";
     slctObjem.style.display = "none";
+    slctObsah.style.display = "none";
+    btnDelka.className = "one aktivni";
+    btnHmotnost.className = "two neaktivni";
     btnObjem.className = "three neaktivni";
+    btnObsah.className = "four neaktivni";
   };
 
   btnHmotnost.onclick = function () {
     slctDelka.style.display = "none";
-    btnDelka.className = "one neaktivni";
-    btnHmotnost.className = "two aktivni";
     slctHmotnost.style.display = "inline";
     slctObjem.style.display = "none";
+    slctObsah.style.display = "none";
+    btnDelka.className = "one neaktivni";
+    btnHmotnost.className = "two aktivni";
     btnObjem.className = "three neaktivni";
+    btnObsah.className = "four neaktivni";
   };
 
   btnObjem.onclick = function () {
-    slctObjem.style.display = " inline";
     slctDelka.style.display = "none";
     slctHmotnost.style.display = "none";
-    btnObjem.className = "three aktivni";
+    slctObjem.style.display = " inline";
+    slctObsah.style.display = "none";
     btnDelka.className = "one neaktivni";
     btnHmotnost.className = "two neaktivni";
+    btnObjem.className = "three aktivni";
+    btnObsah.className = "four neaktivni";
+  };
+
+  btnObsah.onclick = function () {
+    slctDelka.style.display = "none";
+    slctHmotnost.style.display = "none";
+    slctObjem.style.display = "none";
+    slctObsah.style.display = "inline";
+    btnDelka.className = "one neaktivni";
+    btnHmotnost.className = "two neaktivni";
+    btnObjem.className = "three neaktivni";
+    btnObsah.className = "four aktivni";
   };
 };
