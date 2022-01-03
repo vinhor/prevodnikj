4356bf8e15fc3e98ee118ff08808cfe0779e8986
Vincent Horecký <vincent.horecky@seznam.cz>
2022-01-03 19:14:52 +0100

    Zprovozněna teplota

--- JS/main.js
+++ JS/main.js
@@ -4,11 +4,13 @@ window.onload = function () {
   let slctObjem = document.getElementById("slctObjem");
   let slctObsah = document.getElementById("slctObsah");
   let slctRychlost = document.getElementById("slctRychlost");
+  let slctTeplota = document.getElementById("slctTeplota");
   let btnDelka = document.getElementById("btnDelka");
   let btnHmotnost = document.getElementById("btnHmotnost");
   let btnObjem = document.getElementById("btnObjem");
   let btnObsah = document.getElementById("btnObsah");
   let btnRychlost = document.getElementById("btnRychlost");
+  let btnTeplota = document.getElementById("btnTeplota");
 
   btnDelka.onclick = function () {
     slctDelka.style.display = "inline";
@@ -16,11 +18,13 @@ window.onload = function () {
     slctObjem.style.display = "none";
     slctObsah.style.display = "none";
     slctRychlost.style.display = "none";
+    slctTeplota.style.display = "none";
     btnDelka.className = "one aktivni";
     btnHmotnost.className = "two neaktivni";
     btnObjem.className = "three neaktivni";
     btnObsah.className = "four neaktivni";
     btnRychlost.className = "five neaktivni";
+    btnTeplota.className = "six neaktivni";
   };
 
   btnHmotnost.onclick = function () {
@@ -29,11 +33,13 @@ window.onload = function () {
     slctObjem.style.display = "none";
     slctObsah.style.display = "none";
     slctRychlost.style.display = "none";
+    slctTeplota.style.display = "none";
     btnDelka.className = "one neaktivni";
     btnHmotnost.className = "two aktivni";
     btnObjem.className = "three neaktivni";
     btnObsah.className = "four neaktivni";
     btnRychlost.className = "five neaktivni";
+    btnTeplota.className = "six neaktivni";
   };
 
   btnObjem.onclick = function () {
@@ -42,11 +48,13 @@ window.onload = function () {
     slctObjem.style.display = " inline";
     slctObsah.style.display = "none";
     slctRychlost.style.display = "none";
+    slctTeplota.style.display = "none";
     btnDelka.className = "one neaktivni";
     btnHmotnost.className = "two neaktivni";
     btnObjem.className = "three aktivni";
     btnObsah.className = "four neaktivni";
     btnRychlost.className = "five neaktivni";
+    btnTeplota.className = "six neaktivni";
   };
 
   btnObsah.onclick = function () {
@@ -55,11 +63,13 @@ window.onload = function () {
     slctObjem.style.display = "none";
     slctObsah.style.display = "inline";
     slctRychlost.style.display = "none";
+    slctTeplota.style.display = "none";
     btnDelka.className = "one neaktivni";
     btnHmotnost.className = "two neaktivni";
     btnObjem.className = "three neaktivni";
     btnObsah.className = "four aktivni";
     btnRychlost.className = "five neaktivni";
+    btnTeplota.className = "six neaktivni";
   };
 
   btnRychlost.onclick = function () {
@@ -68,10 +78,27 @@ window.onload = function () {
     slctObjem.style.display = "none";
     slctObsah.style.display = "none";
     slctRychlost.style.display = "inline";
+    slctTeplota.style.display = "none";
     btnDelka.className = "one neaktivni";
     btnHmotnost.className = "two neaktivni";
     btnObjem.className = "three neaktivni";
     btnObsah.className = "four neaktivni";
     btnRychlost.className = "five aktivni";
+    btnTeplota.className = "six neaktivni";
+  };
+
+  btnTeplota.onclick = function () {
+    slctDelka.style.display = "none";
+    slctHmotnost.style.display = "none";
+    slctObjem.style.display = "none";
+    slctObsah.style.display = "none";
+    slctRychlost.style.display = "none";
+    slctTeplota.style.display = "inline";
+    btnDelka.className = "one neaktivni";
+    btnHmotnost.className = "two neaktivni";
+    btnObjem.className = "three neaktivni";
+    btnObsah.className = "four neaktivni";
+    btnRychlost.className = "five neaktivni";
+    btnTeplota.className = "six aktivni";
   };
 };
