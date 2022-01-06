b801172a6de954bd9fd3fc3b2582cba935e168b8
Vincent Horecký <vincent.horecky@seznam.cz>
2022-01-06 15:24:10 +0100

    ++ kmh-ms

--- main.js
+++ main.js
@@ -787,4 +787,17 @@ window.onload = function () {
     inpHodnota = document.getElementById("inpHodnota");
     naparsovano = null;
   }
+
+  function kmhNaMs() {
+    inpHodnota = inpHodnota.value.trim();
+    naparsovano = parseFloat(inpHodnota);
+    if (isNaN(naparsovano) || naparsovano == null) {
+      upozorneni.innerHTML = "Nezadali jste číslo!";
+    } else {
+      vysledek = naparsovano / 3.6;
+      upozorneni.innerHTML = naparsovano + " km/h je " + vysledek + " m/s";
+    }
+    inpHodnota = document.getElementById("inpHodnota");
+    naparsovano = null;
+  }
 };
